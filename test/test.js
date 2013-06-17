c5bf1bc06c25452e3505026783694ed6853eea6d
luofei2011 <luofeihit2010@gmail.com>
2013-06-17 17:50:20 +0800

    测试单元

--- test/test.js
+++ test/test.js
@@ -5,17 +5,22 @@ var http=require("http");
 var querystring=require("querystring");
 var fs = require('fs');
 
-var options={
-	host:"localhost",
-	path:"/?CID=1103102",
-	method:"get",
-    port: '6439',
-	headers:{
-		"Content-Type":"application/x-www-form-urlencoded; charset=utf8",
-	}
-};
+// 遍历班号, 做数据缓存
+var content = fs.readFileSync('./CID', 'utf8').split('\n');
+var i = 0;
 
 var oTimer = setInterval(function() {
+    if (i > content.length) 
+        clearInterval(oTimer);
+    var options={
+        host:"localhost",
+        path:"/?CID=" + content[i++],
+        method:"get",
+        port: '6439',
+        headers:{
+            "Content-Type":"application/x-www-form-urlencoded; charset=utf8",
+        }
+    };
     var req=http.request(options,function(res){
         var result = "";
         res.setEncoding("utf8");
